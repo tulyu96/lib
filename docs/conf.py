1085fa2ffdfbd15a43fa316e298bab171ef1852e
tulyu96 <66899229+tulyu96@users.noreply.github.com>
2023-05-29 00:04:50 -0500

    feat: add new configs for ReadDocs to build

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,20 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+
+project = "qrlib"
+copyright = "2023, Tu"
+author = "Tu"
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
 extensions = [
     "myst_nb",
     "autoapi.extension",
@@ -6,3 +23,6 @@ extensions = [
 ]
 autoapi_dirs = ["../qrlib"]  # location to parse for API reference
 html_theme = "sphinx_rtd_theme"  #
+
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
